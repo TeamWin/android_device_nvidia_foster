df75039e2588f0f8a60d262f0c74985613e9634e
Aaron Kling <webgeek1234@gmail.com>
2016-05-30 01:09:45 -0500

    Switch to omni build environment

--- omni_foster.mk
+++ omni_foster.mk
@@ -1,8 +1,8 @@
 # Inherit device configuration for foster.
 $(call inherit-product, device/nvidia/foster/full_foster.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tv.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
 
-PRODUCT_NAME := cm_foster
+PRODUCT_NAME := omni_foster
 PRODUCT_DEVICE := foster
